abeed91f267abbfca162a77b057c9367139d3354
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-05 12:15:39 +0530

    .

---
+++ Axios/AxiosRequestCancellation.js
@@ -0,0 +1,23 @@
+import React from "react";
+import axios from 'axios';
+function App(){
+  const source=axios.CancelToken.source();
+  axios.get('https://backend-b4hi.onrender.com/api/portfolio',{cancelToken:source.token})
+  .then(response=>{
+    console.log(response.data);
+  })
+  .catch(error=>{
+    if(axios.isCancel(error)){
+      console.log("Fetching Canceled By user ")
+    }
+    else{
+      console.log("helllo")
+    }
+  })
+  source.cancel("Request Canceled by user")
+ return(
+  <>
+  </>
+ )
+}
+export default App;
